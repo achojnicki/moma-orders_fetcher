80a8bef5475930a68a0336447d19340a0fa11d26
Adrian Chojnicki <adrian@chojnicki.info>
2024-05-08 16:55:52 +0100

    changes

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
-from adisconfig import adisconfig
-from log import Log
+from adistools.adisconfig import adisconfig
+from adistools.log import Log
 
 from pika import BlockingConnection, PlainCredentials, ConnectionParameters
 from websockets.sync.client import connect as websocket_connect
